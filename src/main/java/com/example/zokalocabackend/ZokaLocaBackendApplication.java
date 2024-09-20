b3b921178cb4f7d607d384f26daa2454f1575bc5
Arjan Gosens <a.gosens@student.fontys.nl>
2024-09-20 15:19:24 +0200

    Initial commit

---
+++ src/main/java/com/example/zokalocabackend/ZokaLocaBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.zokalocabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZokaLocaBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZokaLocaBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/zokalocabackend/ZokaLocaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.zokalocabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZokaLocaBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
