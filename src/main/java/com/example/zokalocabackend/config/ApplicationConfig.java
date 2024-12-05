14d8c8b2b711289ed86a5997240dd8485ddfbf16
Arjan Gosens <a.gosens@student.fontys.nl>
2024-12-05 14:13:17 +0100

    Added frontend to docker-compose stack

--- src/main/java/com/example/zokalocabackend/config/ApplicationConfig.java
+++ src/main/java/com/example/zokalocabackend/config/ApplicationConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableMethodSecurity
 @Configuration
